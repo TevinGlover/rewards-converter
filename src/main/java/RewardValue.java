ed1924aa876194f6f99ab9bfe25eb2e6cdd98057
Tevin Glover <tevwork.tg@gmail.com>
2023-12-13 11:15:38 -0500

    done

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,25 +1,33 @@
 public class RewardValue {
-    Double cashValue;
+   private Double cashValue;
     int milesValue;
+   public static final double milesToCashRateOfExchange = 0.0035; // RewardValue must convert from miles to cash at a rate of 0.0035.
     public RewardValue(Double cashValue){ // constructor1
         this.cashValue = cashValue;
 
     }
 
     public RewardValue(int milesValue){ // constructor2
-        this.milesValue = milesValue;
+        this.cashValue = convertMilesToCash(milesValue);
 
     }
 
-    public double getCashValue() {return cashValue;
+    public double getCashValue() {return this.cashValue;
     }
 
-    public int getMilesValue() {return milesValue;
+    public int getMilesValue() {
+        return convertCashToMiles(this.cashValue);
+    }
+
+    public static double convertMilesToCash(int milesValue) {
+        return milesValue * milesToCashRateOfExchange;
+    }
+    public static int convertCashToMiles(double cashValue){
+        return  (int) (cashValue / milesToCashRateOfExchange);
+       }
+
+
 
     }
 
-    // RewardValue must convert from miles to cash at a rate of 0.0035.
 
-
-
-}
