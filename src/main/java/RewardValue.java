c3859a89834bb264bea9d566c6cba9515a3f0718
Tevin Glover <tevwork.tg@gmail.com>
2023-12-12 23:13:56 -0500

    constructor done

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,2 +1,18 @@
 public class RewardValue {
+    Double cashValue;
+    int milesValue;
+    public RewardValue(Double cashValue){ // constructor1
+        this.cashValue = cashValue;
+
+    }
+    public RewardValue(int milesValue){ // constructor2
+        this.milesValue = milesValue;
+
+    }
+
+    public double getCashValue() {return cashValue;
+    }
+
+    public int getMilesValue() {return milesValue;
+    }
 }
